e782afc2726308e179951bfe516f502624f2f054
HatemGr <hatemgribi@gmail.com>
2022-06-06 14:08:36 +0200

    user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,24 @@ class UsersController < ApplicationController
   end
 
   def edit 
+    puts params
 
   end
 
   def update 
-
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_path(@user), notice: "Modifications enregistrées" }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
+
     def set_user
       @user = User.find(params[:id])
     end
