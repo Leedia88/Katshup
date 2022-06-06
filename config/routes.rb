22772dad701e3871516aed2f8981a5af10dffbd7
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-06-06 21:20:09 +0200

    fix bug buttons

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     resources :orders , only: [:new, :create, :update]
   end
 
-  resources :products , only: [:index, :show] 
+  resources :products , only: [:index, :show, :edit] do
+    resources :images, only: [:create]
+  end
 end
