bc2cc5f0c503fac0541d6e30780124c171eef507
HatemGr <hatemgribi@gmail.com>
2022-06-06 15:28:24 +0200

    action mailer added, work on dev

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,13 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'no_reply-katshup@yopmail.com'
  
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    @url  = 'http://katshup.fr/login' 
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create  :set_cart
+  after_create  :set_cart, :welcome_send
+
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -12,6 +13,10 @@ class User < ApplicationRecord
   def set_cart
     Cart.create!(user: self)
     # redirect_to root_path
+  end
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
   end
 
 
