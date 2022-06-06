fe00538bac3d6dc736cc3814fc7a3dfe32ea5574
HatemGr <hatemgribi@gmail.com>
2022-06-06 13:02:23 +0200

    added user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :authenticate_user, only: [:edit, :show, :update]
+
+  def show 
+
+  end
+
+  def edit 
+
+  end
+
+  def update 
+
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :encrypted_password, :description, :first_name, :last_name)
+    end
+
+    def authenticate_user
+      unless current_user
+        flash[:danger] = "Please log in."
+        redirect_to new_user_registration_path 
+      end
+    end
+
+end
