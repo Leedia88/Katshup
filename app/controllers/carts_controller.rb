ea9e10d840956d86bc9a6e5e6dd573102666e51a
HatemGr <hatemgribi@gmail.com>
2022-06-07 07:37:40 +0200

    added empty cart button

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,23 @@
 class CartsController < ApplicationController
+    before_action :authenticate_user, only: [:edit, :show, :update, :new]
+
     def show
         @cart = current_user.cart
-         @products = @cart.get_products
+        @products = @cart.get_products
+    end
+
+    def destroy 
+        @cart = current_user.cart
+        CartProduct.where(cart:@cart).destroy_all
+    end
+
+    private
+
+    def authenticate_user
+      unless current_user
+        flash[:danger] = "Please log in."
+        redirect_to new_user_registration_path 
+      end
     end
 
 end
