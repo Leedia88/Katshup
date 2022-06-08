b39b59e17022aafe1f18bfb4a902d2f9c058df3e
HatemGr <hatemgribi@gmail.com>
2022-06-08 17:27:58 +0200

    model evaluation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :cart, dependent: :destroy
+  has_many :evaluations
   has_many :orders
   has_one_attached :avatar, dependent: :destroy
 
---
+++ db/migrate/20220608143850_create_evaluations.rb
@@ -0,0 +1,11 @@
+class CreateEvaluations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :evaluations do |t|
+      t.references :user, foreign_key: true
+      t.references :product, foreign_key: true
+      t.float :grade
+
+      t.timestamps
+    end
+  end
+end
