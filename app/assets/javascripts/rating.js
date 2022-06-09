7ad4f19053e047d122bad762e28bc781cb7a1ba5
HatemGr <hatemgribi@gmail.com>
2022-06-09 09:38:49 +0200

    commented rating.js

--- app/assets/javascripts/rating.js
+++ app/assets/javascripts/rating.js
@@ -1,26 +1,26 @@
-const starEditPage = document.querySelector("[data-startEditPage]")
+// const starEditPage = document.querySelector("[data-startEditPage]")
 
-const grade = () => {
-  let stars = document.querySelector(".star-grade").childNodes;
+// const grade = () => {
+//   let stars = document.querySelector(".star-grade").childNodes;
 
-  stars.forEach(button => {button.addEventListener('mouseover', () => {
-    button.firstElementChild.classList.add('fa-solid');
-    button.firstElementChild.classList.remove('fa-regular');
-    let e = button.previousElementSibling;
-    let j = button.nextElementSibling;
-    while (e != null) {
-      e.firstElementChild?.classList.add('fa-solid');
-      e.firstElementChild?.classList.remove('fa-regular');
-      e = e.previousElementSibling;
-    }
-    while (j != null) {
-      j.firstElementChild?.classList.add('fa-regular');
-      j.firstElementChild?.classList.remove('fa-solid');
-      j = j.nextElementSibling;
-    }
-  })})
-}
+//   stars.forEach(button => {button.addEventListener('mouseover', () => {
+//     button.firstElementChild.classList.add('fa-solid');
+//     button.firstElementChild.classList.remove('fa-regular');
+//     let e = button.previousElementSibling;
+//     let j = button.nextElementSibling;
+//     while (e != null) {
+//       e.firstElementChild?.classList.add('fa-solid');
+//       e.firstElementChild?.classList.remove('fa-regular');
+//       e = e.previousElementSibling;
+//     }
+//     while (j != null) {
+//       j.firstElementChild?.classList.add('fa-regular');
+//       j.firstElementChild?.classList.remove('fa-solid');
+//       j = j.nextElementSibling;
+//     }
+//   })})
+// }
 
-if (starEditPage != null) {
-  grade();
-}
+// if (starEditPage != null) {
+//   grade();
+// }
