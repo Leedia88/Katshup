9bacb84b80438c604fdb9b7ce6e7333b217bfd5d
HatemGr <hatemgribi@gmail.com>
2022-06-09 13:41:30 +0200

    readjusted filtering

--- app/assets/javascripts/example.js
+++ app/assets/javascripts/example.js
@@ -1,20 +1,36 @@
 var checkboxes = document.querySelectorAll(".category-checkboxes");
 var cards = document.querySelectorAll(".cardy-container");;
 
+function otherBoxesUnchecked(checkbox) {
+	var answer = true;
+	checkboxes.forEach((box) => {
+		if (box.checked && box != checkbox) {
+			answer = false;
+		}
+	});
+	return answer;
+}
+
 checkboxes.forEach((checkbox) => {
 	checkbox.addEventListener("change", () => {
-		cards.forEach((card) => {
-			card.style.display = "none";
-			var stayHidden = true;
-			var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
-			checkedBoxes.forEach(function(elem) {
-
-				if(card.classList.contains(elem.id)){
-					stayHidden = false;
-				};
-			});
-			if(!stayHidden) {card.style.display = "";}
-		});
-	});
-});
-
+		var checkboxCard = document.querySelectorAll(`.cardy-container.${checkbox.getAttribute("id")}`);
+		if (otherBoxesUnchecked(checkbox)) {
+			if(checkbox.checked){
+			cards.forEach((card) => {card.classList.add("collapse")});
+			checkboxCard.forEach((card) => {card.classList.remove("collapse")});
+			} else {
+				cards.forEach((card) => {card.classList.remove("collapse")});
+			}
+		} else {
+			if(checkbox.checked){
+				checkboxCard.forEach((card) => {card.classList.remove("collapse")});
+			} else {
+				checkboxCard.forEach((card) => {card.classList.add("collapse")});
+				checkboxes.forEach((checkbox) => {
+				if(checkbox.checked) {
+					var checkboxCard = document.querySelectorAll(`.cardy-container.${checkbox.getAttribute("id")}`);
+					checkboxCard.forEach((card) => {card.classList.remove("collapse")});
+				}})
+			}
+		}
+	})});
